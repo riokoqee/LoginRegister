3e830061a45ed8234babb930d21dd43afc4b4f22
rioko <rioko988@gmail.com>
2025-03-16 19:14:27 +0500

    Fix routing issue

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,21 @@ import React, { useState } from "react";
 import Login from "./components/Login";
 import Profile from "./components/Profile";
 import Register from "./components/Register";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/" element={<Login />} />
-        </Routes>
-      </Router>
+      <HashRouter>
+        <Router>
+          <Routes>
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/" element={<Login />} />
+          </Routes>
+        </Router>
+      </HashRouter>
     </>
   )
 }
